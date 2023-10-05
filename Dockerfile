1ced64fedcc1bc3b490b51a704f1b707eb5a2461
nanidevops <nanidevops999@gmail.com>
2023-10-05 19:18:55 +0530

    shell

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM mdsol/java17-jre:latest
-COPY target/StoreAPI.jar StoreAPI.jar
-EXPOSE 6500
-ENTRYPOINT ["java","-jar","StoreAPI.jar","&"]
---
+++ start-app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shellscript to stop running existing jar and re run new jar
+################ Kill Running JAR ####################
+ps -ef  | grep 'StoreAPI.jar'
+sudo pkill -f StoreAPI.jar
+
+################ BACKUP OLD JAR ####################
+echo "Backup old jar"
+sudo mv /jenkins/workspace/Ishop_JAVA_API/target/StoreAPI.jar /jenkins/backup/StoreAPI.jar_$(date +%F-%H:%M:%S).jar
+
+
+################ START NEW JAR ####################
+echo "Start new application"
+cd /jenkins/workspace/Ishop_JAVA_API/target
+sudo java -jar StoreAPI.jar &
+
+echo "Sleep 5 sec"
+sleep 5s
+
+ps -ef  | grep 'StoreAPI.jar'
+echo "Deployed StoreAPI JAR successfully"
