b12d297b6517ee31bbd5423fe297dad2510bab69
nanidevops <nanidevops999@gmail.com>
2023-10-05 20:04:18 +0530

    shell

--- start-app.sh
+++ start-app.sh
@@ -8,6 +8,7 @@ sudo pkill -f StoreAPI.jar
 ################ BACKUP OLD JAR ####################
 echo "################ BACKUP OLD JAR ####################"
 sudo mv /jenkins/release/StoreAPI.jar /jenkins/backup/StoreAPI.jar_$(date +%F-%H:%M:%S).jar
+ls -ltr /jenkins/backup
 echo "################ BACKUP SUCCESS ####################"
 
 ################ STARTING STORE APP ####################
@@ -15,10 +16,11 @@ echo "################ STARTING STORE APP ####################"
 mv /jenkins/staging/StoreAPI.jar /jenkins/release
 cd /jenkins/release
 sudo java -jar StoreAPI.jar &
-echo "################ STARTING STORE APP RUNNING ####################"
-
 echo "Sleep 5 sec"
 sleep 5s
+ls -ltr /jenkins/release
+echo "################ STORE APP RUNNING ####################"
+
 
 ps -ef  | grep 'StoreAPI.jar'
 echo "Deployed StoreAPI JAR successfully"
