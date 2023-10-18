8bd7fa4be7602a1460396a5ef9092c43bab8074d
nani <kadurunarendra@gmail.com>
2023-10-19 01:41:39 +0530

    update

--- src/main/java/com/storeapi/demo/StoreApiApplication.java
+++ src/main/java/com/storeapi/demo/StoreApiApplication.java
@@ -2,6 +2,9 @@ package com.storeapi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StoreApiApplication {
@@ -9,5 +12,15 @@ public class StoreApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 }
