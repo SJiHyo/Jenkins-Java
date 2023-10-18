0da47e7d3c3e1159e20199f37db76734bfb44fa1
nani <kadurunarendra@gmail.com>
2023-10-19 01:59:35 +0530

    update

--- src/main/java/com/storeapi/demo/CartController/CartController.java
+++ src/main/java/com/storeapi/demo/CartController/CartController.java
@@ -1,6 +1,7 @@
 package com.storeapi.demo.CartController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import com.storeapi.demo.Cart.Cart;
 import com.storeapi.demo.CartRepository.CartRepository;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class CartController {
 	
 	@Autowired
--- src/main/java/com/storeapi/demo/CustomerController/CustomerController.java
+++ src/main/java/com/storeapi/demo/CustomerController/CustomerController.java
@@ -3,6 +3,7 @@ package com.storeapi.demo.CustomerController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.storeapi.demo.Customer.Customer;
 import com.storeapi.demo.CustomerRepository.CustomerRepository;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRepository;
